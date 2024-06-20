1318635ae81e1f44dc49b4b1ba065d2ff5a6f272
Austin Marlatt <austin.marlatt1@gmail.com>
2024-06-19 17:10:43 -0700

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import Nav from ('./Nav');
+
+export default function header(){
+
+  return (
+    <div className="header">
+      <h1>Austin Marlatt</h1>
+    
+      <Nav />
+    
+    </div>
+  );
+}
---
+++ src/components/Project.jsx
@@ -0,0 +1,15 @@
+import Header from './Header';
+import Portfolio from './Portfolio';
+import Footer from './Footer';
+
+export default function Project() {
+  return (
+    <nav>
+      <Header />
+  
+  
+  
+      <Footer />
+    </nav>
+  );
+}
