a716ac856cb0a6bcb12886e6c3d3d08b21eccfc3
Austin Marlatt <austin.marlatt1@gmail.com>
2024-06-26 20:29:32 -0700

    Added content to the pages and final styling

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,25 +1,31 @@
 export default function Resume() {
   return(
     <div>
-      <p>Download my Resume <a href="src\assets\resume\Austin-Marlatt-Resume.pdf" download="Austin-Marlatt-Resume.pdf">here.</a></p>
-      <h3>I am proficient in the MERN Development stack.</h3>
-      <ul>
-        <li>MongoDB</li>
-        <li>Express</li>
-        <li>React</li>
-        <li>Node</li>
-      </ul>
-      <h3>
-        Languages and Software:
-      </h3>
-      <ul>
-        <li>HTML</li>
-        <li>JavaScript</li>
-        <li>CSS</li>
-        <li>Python</li>
-        <li>SQL</li>
-        <li>React</li>
-      </ul>
+
+      <div>
+        <h3>I am proficient in the MERN Development stack.</h3>
+        <ul className="list-group">
+          <li className="list-group-item">MongoDB</li>
+          <li className="list-group-item">Express</li>
+          <li className="list-group-item">React</li>
+          <li className="list-group-item">Node</li>
+        </ul>
+      </div>
+
+      <div>
+        <h3>
+          Languages and Software:
+        </h3>
+        <ul className="list-group">
+          <li className="list-group-item">HTML</li>
+          <li className="list-group-item">JavaScript</li>
+          <li className="list-group-item">CSS</li>
+          <li className="list-group-item">Python</li>
+          <li className="list-group-item">SQL</li>
+        </ul>
+      </div>
+      
+      <p>Download my Resume <a href="" download="Austin-Marlatt-Resume.pdf">here.</a></p>
     </div>
   );
 }
